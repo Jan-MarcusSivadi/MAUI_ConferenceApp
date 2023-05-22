f7c9ed56f965840d6b73df4400c695b61d38ea18
Jan-MarcusSivadi <jmarcus@hot.ee>
2023-05-22 15:25:04 +0300

    add Session Model

---
+++ AppConference/Models/Session.cs
@@ -0,0 +1,14 @@
+﻿namespace AppConference.Models;
+
+public class Session
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Room { get; set; }
+    public string Description { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public string StartTimeDisplay => $"{Start:t}";
+    public string DayDisplay => $"{Start:MMM} {Start:d}";
+}
+
