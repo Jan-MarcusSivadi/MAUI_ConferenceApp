748fe59f1b7460652c4d38e744fef8b5c1da9d72
Jan-MarcusSivadi <jmarcus@hot.ee>
2023-05-22 15:31:17 +0300

    add Speakers Model

--- AppConference/Models/Session.cs
+++ AppConference/Models/Session.cs
@@ -1,14 +1,23 @@
-﻿namespace AppConference.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class Session
+namespace AppConference.Models;
+
+public class Session : ObservableObject
 {
+    // Static
     public int Id { get; set; }
     public string Title { get; set; }
     public string Room { get; set; }
     public string Description { get; set; }
+    public bool InAgenda { get; set; }
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
     public string StartTimeDisplay => $"{Start:t}";
     public string DayDisplay => $"{Start:MMM} {Start:d}";
+    public Speaker Speaker { get; set; }
+
+    // Dynamic
+    [ObservableProperty]
+    bool inAgenda;
 }
 
