aaaf5834c5f35f347515799ff6cffb28996b7790
Jan-MarcusSivadi <jmarcus@hot.ee>
2023-05-22 14:42:48 +0300

    Create Basic Pages

---
+++ AppConference/Pages/AgendaPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppConference.Pages;
+
+public partial class AgendaPage : ContentPage
+{
+	public AgendaPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppConference/Pages/SchedulePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppConference.Pages;
+
+public partial class SchedulePage : ContentPage
+{
+	public SchedulePage()
+	{
+		InitializeComponent();
+	}
+}
